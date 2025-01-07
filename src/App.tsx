c9e4b91eb229e55aebbe0d1aed0ab7a59ebe0957
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2025-01-07 11:32:09 -0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
